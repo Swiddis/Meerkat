f20e766debe10a7f65d5fd5b4f1c1465c554ced4
Travja <the.only.t.craft@gmail.com>
2022-03-04 00:36:33 -0700

    Well... there's some code that isn't fully hooked up yet.
    Not sure that registration (and potentially sign in) should necessarily be on the backend actually. There's a fair amount of interaction with session management that can happen on the frontend that might be nice to have there as opposed to the backend.

--- stacks/MeerkatStack.js
+++ stacks/MeerkatStack.js
@@ -4,71 +4,85 @@ import * as apigAuthorizers from "@aws-cdk/aws-apigatewayv2-authorizers-alpha";
 // import {PermissionType} from "@serverless-stack/resources";
 
 export default class MeerkatStack extends sst.Stack {
-    constructor(scope, id, props) {
-        super(scope, id, props);
+  constructor(scope, id, props) {
+    super(scope, id, props);
 
-        // Create the tables
-        const projectTable = new sst.Table(this, "Project", {
-            fields: {
-                id: sst.TableFieldType.STRING,
-                name: sst.TableFieldType.STRING,
-                admin: sst.TableFieldType.STRING, // TODO flesh out permissions
-                users: [sst.TableFieldType.STRING]
-            },
-            primaryIndex: {partitionKey: "id"}
-        });
+    // Create the tables
+    const projectTable = new sst.Table(this, "Project", {
+      fields: {
+        id: sst.TableFieldType.STRING,
+        name: sst.TableFieldType.STRING,
+        admin: sst.TableFieldType.STRING, // TODO flesh out permissions
+        users: [sst.TableFieldType.STRING]
+      },
+      primaryIndex: {partitionKey: "id"}
+    });
 
-        const ticketTable = new sst.Table(this, "Ticket", {
-            fields: {
-                id: sst.TableFieldType.STRING,
-                project: sst.TableFieldType.STRING,
-                author: sst.TableFieldType.STRING,
-                timestamp: sst.TableFieldType.STRING,
-                assigned_to: sst.TableFieldType.STRING,
-                status: sst.TableFieldType.STRING,
-                resolution: sst.TableFieldType.STRING,
-                type: sst.TableFieldType.STRING,
-                severity: sst.TableFieldType.NUMBER,
-                priority: sst.TableFieldType.NUMBER,
-                title: sst.TableFieldType.STRING,
-                description: sst.TableFieldType.STRING,
-                reproduction_steps: sst.TableFieldType.STRING,
-                expected_result: sst.TableFieldType.STRING,
-            },
-            primaryIndex: {partitionKey: "id"},
-        });
+    const ticketTable = new sst.Table(this, "Ticket", {
+      fields: {
+        id: sst.TableFieldType.STRING,
+        project: sst.TableFieldType.STRING,
+        author: sst.TableFieldType.STRING,
+        timestamp: sst.TableFieldType.STRING,
+        assigned_to: sst.TableFieldType.STRING,
+        status: sst.TableFieldType.STRING,
+        resolution: sst.TableFieldType.STRING,
+        type: sst.TableFieldType.STRING,
+        severity: sst.TableFieldType.NUMBER,
+        priority: sst.TableFieldType.NUMBER,
+        title: sst.TableFieldType.STRING,
+        description: sst.TableFieldType.STRING,
+        reproduction_steps: sst.TableFieldType.STRING,
+        expected_result: sst.TableFieldType.STRING,
+      },
+      primaryIndex: {partitionKey: "id"},
+    });
 
-        const userTable = new sst.Table(this, "User", {
-            fields: {
-                id: sst.TableFieldType.STRING,
-                username: sst.TableFieldType.STRING,
-                email: sst.TableFieldType.STRING
-            },
-            primaryIndex: {partitionKey: "id"}
-        });
+    const userTable = new sst.Table(this, "User", {
+      fields: {
+        id: sst.TableFieldType.STRING,
+        username: sst.TableFieldType.STRING,
+        email: sst.TableFieldType.STRING
+      },
+      primaryIndex: {partitionKey: "id"}
+    });
 
     const queue = new sst.Queue(this, "EmailQueue", {
-      consumer: "src/email_consumer.consumeEmail",
-      permissions: ["s3", "ses"]
+      consumer: "src/email_consumer.consumeEmail"
     });
     queue.attachPermissions(sst.PermissionType.ALL);
 
     const userPool = new cognito.UserPool(this, "UserPool", {
       selfSignUpEnabled: true,
-      signInAliases: { email: true },
-      signInCaseSensitive: false
+      signInAliases: {email: true},
+      signInCaseSensitive: false,
+      customAttributes: {
+        "username": new cognito.StringAttribute()
+      }
     });
 
     const userPoolClient = new cognito.UserPoolClient(this, "UserPoolClient", {
       userPool,
-      authFlows: { userPassword: true },
+      authFlows: {userPassword: true},
     });
+
+    const defaultAuthorizer = new apigAuthorizers.HttpUserPoolAuthorizer("Authorizer", userPool, {
+      userPoolClients: [userPoolClient]
+    });
+    const defaultAuthorizationType = sst.ApiAuthorizationType.JWT;
+
+    const buildAuthenticatedEndpoint = (handler) => {
+      return {
+        function: handler,
+        authorizer: defaultAuthorizer,
+        authorizationType: defaultAuthorizationType,
+      };
+    };
 
     // Create a HTTP API
     const api = new sst.Api(this, "Api", {
-      defaultAuthorizer: new apigAuthorizers.HttpUserPoolAuthorizer("Authorizer", userPool, {
-        userPoolClients: [userPoolClient]
-      }),
+      // defaultAuthorizer,
+      // defaultAuthorizationType,
       defaultFunctionProps: {
         // Allow the API to access the table
         permissions: [projectTable, ticketTable, userTable],
@@ -77,12 +91,18 @@ export default class MeerkatStack extends sst.Stack {
           projectTableName: projectTable.dynamodbTable.tableName,
           ticketTableName: ticketTable.dynamodbTable.tableName,
           userTableName: userTable.dynamodbTable.tableName,
-          queueUrl: queue.sqsQueue.queueUrl
+          queueUrl: queue.sqsQueue.queueUrl,
+          userPoolId: userPool.userPoolId,
+          userPoolClientId: userPoolClient.userPoolClientId
         },
       },
-      defaultAuthorizationType: sst.ApiAuthorizationType.JWT,
       routes: {
-        "GET /ticket": "src/ticket_lambda.getTickets",
+        /*
+         Maybe this should be authenticated... Maybe not.
+         The point is, we can (and maybe should) authenticate
+         individual endpoints rather than the lot.
+        */
+        "GET /ticket": buildAuthenticatedEndpoint("src/ticket_lambda.getTickets"),
         "GET /ticket/{id}": "src/ticket_lambda.getTicket",
         "POST /ticket": "src/ticket_lambda.createTicket",
         "DELETE /ticket/{id}": "src/ticket_lambda.deleteTicket",
@@ -97,7 +117,10 @@ export default class MeerkatStack extends sst.Stack {
         "POST /project": "src/project_lambda.createProject",
         "DELETE /project/{id}": "src/project_lambda.deleteProject",
         "PUT /project/{id}": "src/project_lambda.updateProject",
-        "POST /email": "src/email_lambda.postEmail"
+        "POST /email": "src/email_lambda.postEmail",
+
+        // "POST /signup": "src/user_lambda.signUpUser",
+        // "POST /confirm": "src/user_lambda.confirmUser",
       },
     });
 
