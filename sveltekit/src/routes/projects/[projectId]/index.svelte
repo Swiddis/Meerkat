f74b19f849bb88936214e6561f93ca588d583d42
Travja <the.only.t.craft@gmail.com>
2022-03-06 22:48:29 -0700

    Account for errors

--- sveltekit/src/routes/projects/[projectId]/index.svelte
+++ sveltekit/src/routes/projects/[projectId]/index.svelte
@@ -1,47 +1,57 @@
-<script context="module" lang="ts">
-    import type {Project} from "$lib/structs";
+<script context='module' lang='ts'>
+	import type { Project } from '$lib/structs';
 
-    export const load = async (page) => {
-        let projectId = page.params.projectId;
-        let res = await fetch(`${import.meta.env.VITE_APP_API_URL}/project/${projectId}`)
-        let project: Project = await res.json();
+	export const load = async (page) => {
+		let projectId = page.params.projectId;
+		let res = await fetch(`${import.meta.env.VITE_APP_API_URL}/project/${projectId}`);
+		let project: Project = await res.json();
 
-        return {
-            props: {
-                project
-            }
-        };
-    };
+		return {
+			props: {
+				project
+			}
+		};
+	};
 </script>
 
-<script lang="ts">
-    import {onMount} from "svelte";
-    import TicketList from "$lib/TicketList.svelte";
-    import Button from "$lib/ui/Button.svelte";
+<script lang='ts'>
+	import { onMount } from 'svelte';
+	import TicketList from '$lib/TicketList.svelte';
+	import Button from '$lib/ui/Button.svelte';
 
-    let tickets;
-    export let project: Project;
+	let tickets;
+	export let project: Project;
 
-    onMount(() => {
-        fetch(`${import.meta.env.VITE_APP_API_URL}/project/${project.id}/ticket`)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                tickets = data;
-            });
-    });
+	onMount(() => {
+		fetch(`${import.meta.env.VITE_APP_API_URL}/project/${project.id}/ticket`)
+			.then((response) => {
+				if (response.status == 404) {
+					return [];
+				}
+
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+				tickets = data;
+			})
+			.catch(err => {
+				console.log(err);
+				alert('An error occurred while getting tickets...');
+			});
+	});
 </script>
 
-<a href="../" class="back" alt="Back to Projects">&#8592; Back to Projects</a>
-<div class="header">
-    <h1>Tickets</h1>
-    <span class="spacer"/>
-    <a href="ticket/create">
-        <Button color={"#4ad028"} fontColor="white">Create a Ticket</Button>
-    </a>
+<a href='../' class='back' alt='Back to Projects'>&#8592; Back to Projects</a>
+<div class='header'>
+	<h1>Tickets</h1>
+	<span class='spacer' />
+	<a href='ticket/create'>
+		<Button color={"#4ad028"} fontColor='white'>Create a Ticket</Button>
+	</a>
 </div>
-<hr/>
-<TicketList {tickets} loading={!tickets}/>
+<hr />
+<TicketList {tickets} loading={!tickets} />
 
 <style>
     .header {
--- sveltekit/src/routes/projects/index.svelte
+++ sveltekit/src/routes/projects/index.svelte
@@ -1,42 +1,43 @@
-<script lang="ts">
-    import {onMount} from "svelte";
-    import type {Project} from "$lib/structs";
-    import Button from "$lib/ui/Button.svelte";
+<script lang='ts'>
+	import { onMount } from 'svelte';
+	import type { Project } from '$lib/structs';
+	import Button from '$lib/ui/Button.svelte';
+	import Loading from '$lib/Loading.svelte';
 
-    let projects: Project[];
+	let projects: Project[];
 
-    onMount(() => {
-        fetch(import.meta.env.VITE_APP_API_URL + "/project")
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                projects = data;
-            });
-    });
+	onMount(() => {
+		fetch(import.meta.env.VITE_APP_API_URL + '/project')
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				projects = data;
+			});
+	});
 </script>
 
-<div class="header">
-    <h1>Projects</h1>
-    <span class="spacer"/>
-    <a href="projects/create">
-        <Button color="#4ad028" fontColor="white">Create Project</Button>
-    </a>
+<div class='header'>
+	<h1>Projects</h1>
+	<span class='spacer' />
+	<a href='create'>
+		<Button color='#4ad028' fontColor='white'>Create Project</Button>
+	</a>
 </div>
-<hr/>
+<hr />
 <div>
-    {#if !projects}
-        Loading...
-    {:else}
-        {#if projects.length == 0}
-            You don't have any projects. <a href="/projects/create" alt="Create a project">Create a project!</a>
-        {:else}
-            {#each projects as project}
-                <a href="/projects/{project.id}">
-                    <h2>{project.name}</h2>
-                </a>
-            {/each}
-        {/if}
-    {/if}
+	{#if !projects}
+		<Loading>Loading...</Loading>
+	{:else}
+		{#if projects.length == 0}
+			You don't have any projects. <a href='create' alt='Create a project'>Create a project!</a>
+		{:else}
+			{#each projects as project}
+				<a href='{project.id}'>
+					<h2>{project.name}</h2>
+				</a>
+			{/each}
+		{/if}
+	{/if}
 </div>
 
 <style>
